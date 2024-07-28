ea6ab107bc5f016dc94c7f408679b46601e0f830
JaraSanchezLuis <107270563+luisenjs@users.noreply.github.com>
2024-07-28 15:32:41 -0500

    Revert "autorización actividad"
    This reverts commit 8cca94520c3f9c5d86fbb06ee1240b8fa1949034.

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-const sequelize = require('../models/index.js').sequelize;
-var initModels = require("../models/init-models");
-const { where } = require('sequelize');
-var models = initModels(sequelize);
-
 /* GET home page. */
-router.get('/', async function(req, res, next) {
-  res.render('login', { title: 'Express' });
-});
-
-router.post("/", async function(rep, res, next) {
-  let {user, passwrd} = req.body;
-  try {
-    const datos = await models.users.findAll({
-      where: {
-        name: {
-          [Op.like]: user
-        },
-        password: {
-          [Op.like]: passwrd
-        }
-      }
-    })
-    res.redirect("/users")
-  } catch(error){
-    res.redirect("/")
-  }
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
